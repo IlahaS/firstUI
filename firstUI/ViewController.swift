be2cc18915b291f4b691df9eeb8b6cb682950639
Ilahe Samedova <ilahesamedova@mail.ru>
2022-05-30 17:54:28 +0400

    Initial Commit

---
+++ firstUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstUI
+//
+//  Created by Ilahe Samedova on 30.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
